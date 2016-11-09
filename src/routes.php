078111c92e1954d5b02f7e0e3effe1a7e2fb18d2
Giovanni Cellie <giovannicweb@gmail.com>
2016-11-09 12:32:25 +0100

    .

--- src/routes.php
+++ src/routes.php
@@ -13,13 +13,26 @@ $app->get('/', function ($request, $response, $args) {
 // starting_id = 0 for first 20 records, [int] for id value. It asks for ids >= starting_id
 // key = API key
 // 
+// 
 $app->add(new RKA\Middleware\IpAddress());
+
 $app->get('/api/take/{starting_id}/{key}', function ($request, $response, $args) use ($container) {
-	return var_dump($request->getAttribute('ip_address'));
-	if ( isset($args['key']) && $args['key'] == $this->get('settings')['api']['key'] ) {
-		$data = $container->get('db')->table('posts')->where('id', '>', $args['starting_id'])->take(20)->get();
-		return $response->withJson($data);
-	} else {
-		return 'error';
-	}
+	// check if server IP is the same as the request one
+	//if ($request->getAttribute('ip_address') === $_SERVER['SERVER_ADDR'] ) {
+		// check the api key val
+		if ( $args['key'] == $this->get('settings')['api']['key'] ) {
+			// get values from starting id (>=)
+			if ($args['starting_id'] == '0') {
+				$data = $container->get('db')->table('posts')->orderBy('id', 'desc')->take(5)->get();
+			} else {
+				$data = $container->get('db')->table('posts')->where('id', '<', $args['starting_id'])->orderBy('id', 'desc')->take(5)->get();
+			}
+			// if everything is ok.. return JSON formatted response
+			if ($data->isEmpty())
+				$data = '';
+			return $response->withJson($data);
+		}
+	//}
+	// return error if something was wrong
+	return 'Error';
 });
