28e00903271ed9d088e9a216dc2b26a66231b34b
Giovanni Cellie <giovannicweb@gmail.com>
2016-11-09 21:35:31 +0100

    .

--- src/routes.php
+++ src/routes.php
@@ -17,16 +17,24 @@ $app->add(new RKA\Middleware\IpAddress());
 // num posts
 $num_posts = 5;
 
-$app->get('/api/take/{starting_id}/{key}', function ($request, $response, $args) use ($container, $num_posts) {
+$app->get('/api/take/{user}/{starting_id}/{key}', function ($request, $response, $args) use ($container, $num_posts) {
 	// check if server IP is the same as the request one
 	//if ($request->getAttribute('ip_address') === $_SERVER['SERVER_ADDR'] ) {
 		// check the api key val
 		if ( $args['key'] == $this->get('settings')['api']['key'] ) {
 			// get values from starting id (>=)
 			if ($args['starting_id'] == '0') {
-				$data = $container->get('db')->table('posts')->orderBy('id', 'desc')->take($num_posts)->get();
+				if ($args['user'] == 'all') {
+					$data = $container->get('db')->table('posts')->orderBy('id', 'desc')->take($num_posts)->get();
+				} else {
+					$data = $container->get('db')->table('posts')->where('name', 'LIKE', $args['user'])->orderBy('id', 'desc')->take($num_posts)->get();
+				}
 			} else {
-				$data = $container->get('db')->table('posts')->where('id', '<', $args['starting_id'])->orderBy('id', 'desc')->take(5)->get();
+				if ($args['user'] == 'all') {
+					$data = $container->get('db')->table('posts')->where('id', '<', $args['starting_id'])->orderBy('id', 'desc')->take($num_posts)->get();
+				} else {
+					$data = $container->get('db')->table('posts')->where('name', 'LIKE', $args['user'])->where('id', '<', $args['starting_id'])->orderBy('id', 'desc')->take($num_posts)->get();
+				}
 			}
 			// if everything is ok.. return JSON formatted response
 			if ($data->isEmpty())
@@ -36,4 +44,12 @@ $app->get('/api/take/{starting_id}/{key}', function ($request, $response, $args)
 	//}
 	// return error if something was wrong
 	return 'Error';
+});
+
+$app->get('/{user}', function($request, $response, $args) use ($app) {
+	if ( $args['user'] == 'a') {
+		return $response->withRedirect('/');
+	}
+	$user = $args['user'];
+	return $this->renderer->render($response, 'index.phtml', compact('user'));
 });
