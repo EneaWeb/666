854ce465a319c15b5104fb17e14226e83e0a1f37
Giovanni Cellie <giovannicweb@gmail.com>
2016-11-09 20:29:27 +0100

    .

--- public/assets/js/debounce.js
+++
@@ -1,5 +0,0 @@
-// Returns a function, that, as long as it continues to be invoked, will not
-// be triggered. The function will be called after it stops being called for
-// N milliseconds. If `immediate` is passed, trigger the function on the
-// leading edge, instead of the trailing.
-function debounce(a,b,c){var d;return function(){var e=this,f=arguments;clearTimeout(d),d=setTimeout(function(){d=null,c||a.apply(e,f)},b),c&&!d&&a.apply(e,f)}
